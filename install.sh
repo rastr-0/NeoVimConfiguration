d4a05ed5949c5ceceb269d87391ccd3103908516
rastr <romanmilko123@gmail,com>
2024-06-28 01:13:28 +0200

    auto-setup bash script

--- install.sh
+++ install.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
+
 check_nvim() {
-  if ! command -v nvim &> /dev/null
-  then
-    sudo snap install nvim --classic
+  if ! command -v nvim &> /dev/null; then
     echo "Installing NeoVim"
+    sudo snap install nvim --classic
   else
     echo "NeoVim is installed, moving on"
   fi
 }
 
-echo $(check_nvim)
+create_nvim_dir() {
+  if [! -d ~/.config/ ]; then
+    echo "Creating ./config/nvim directory"
+    mkdir -p ~/.config/nvim
+  elif [! -d ~/.config/nvim/ ]; then
+    echo "Creating nvim/ inside .config/"
+    mkdir -p ~/.config/nvim
+  fi
+}
+
+move_files_to_config_dir() {
+  echo "Moving cloned files to .config/nvim directory"
+  mv init.lua ~/.config/nvim
+  mv -R lua ~/.config/nvim
+}
+
+check_nvim
+create_nvim_dir
+move_files_to_config_dir

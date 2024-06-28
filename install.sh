63b774a2bce0b835861321f8180a9d43ab23e619
rastr <romanmilko123@gmail,com>
2024-06-28 23:59:27 +0200

    Fully working basic auto-completion scrirpt

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ check_dependencies() {
   for cmd in wget unzip; do
     if ! command -v $cmd &> /dev/null; then
       echo "Installing $cmd..."
-      sudo apt-get install -y $cmd
+      sudo apt-get install -y $cmd &> /dev/null
     fi
   done
 }
@@ -21,20 +21,33 @@ create_nvim_dir() {
     echo "Creating ./config/nvim directory"
     mkdir -p ~/.config/nvim
   elif [ ! -d ~/.config/nvim/ ]; then
-    echo "Creating nvim/ inside .config/"
+    echo "Creating nvim/ inside ~/.config/"
     mkdir -p ~/.config/nvim
   fi
 }
 
 move_files_to_config_dir() {
   echo "Moving cloned files to .config/nvim directory"
-  mv init.lua lua/ README.md ~/.config/nvim
+  mv init.lua lua/ ~/.config/nvim
 }
 
 download_and_unzip_fonts() {
- echo "Downloading and installing Hack Nerd Font..."
- wget -q "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip"
- sudo unzip -q Hack-v3.003-ttf.zip -d /usr/share/fonts/Hack-v3.003-ttf/ 
+  echo "Downloading and installing Hack Nerd Font..."
+  wget -q "https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip"
+  sudo unzip -q Hack-v3.003-ttf.zip -d /usr/share/fonts/Hack-v3.003-ttf/ 
+  sudo fc-cache -f -v &> /dev/null
+}
+
+delete_cloned_repository() {
+  read -p "Do you want to delete the cloned repository?(Yes/No) " user_answer
+  user_answer=$(echo "$user_answer" | tr '[:upper:]' '[:lower:]')
+
+  if [ "$user_answer" == "yes" ]; then
+    echo "Deleting cloned repository, all the necessery files were moved"
+    sudo rm -rf "$(pwd)"
+  else
+    echo "Cloned repository not deleted"
+  fi
 }
 
 check_dependencies
@@ -43,4 +56,6 @@ create_nvim_dir
 move_files_to_config_dir
 download_and_unzip_fonts
 
-echo "Setup complete. Please start Neovim with \"nvim\" command to finish Lazy.nvim plugin installation."
+echo "Setup is done! Please start Neovim with \"nvim\" command to finish the Lazy.nvim plugin installation."
+
+delete_cloned_repository
